a1b41256dce89e5307df0e08f7e78c4fa6189b8f
Chris <52924685+chrishumanitec@users.noreply.github.com>
2022-01-25 19:25:35 +0100

    Added env var message

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const jokes = [
   "A programmer walks to the butcher shop and buys a kilo of meat.  An hour later he comes back upset that the butcher shortchanged him by 24 grams."
 ];
 
-const message = "Joke of the day!";
+const message = process.env.MESSAGE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
